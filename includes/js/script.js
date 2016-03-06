c5319fa4b475731aeab1667d0c2dd7e23a5dae12
defto94 <defto94@gmail.com>
2016-03-06 10:56:15 +0100

    kontakt, glowna

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,10 +1,30 @@
-/*
 
-My Custom JS
-============
+$(function() {
 
-Author:  Brad Hussey
-Updated: August 2013
-Notes:	 Hand coded for Udemy.com
-
-*/
+	$('#sendAlert').click(function(e) {
+	
+		e.preventDefault();
+		
+		$('#successAlert').slideDown();
+		
+		
+	
+	});
+	
+	$("#glownaBody a:contains('Start')").parent().addClass('active');
+	$("#kontaktBody a:contains('Kontakt')").parent().addClass('active');
+	
+	//wysuwane menu jako hover
+	$('ul.nav li.dropdown').hover(function() {
+	
+		$('.dropdown-menu', this).fadeIn();
+		
+	}, function() {
+	
+		$('.dropdown-menu',this).fadeOut('fast');
+	
+	});
+	
+});
+	
+	
