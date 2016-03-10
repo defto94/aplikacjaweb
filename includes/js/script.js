77363fb5f5d52a167e75f55f76b3287f539a5cdf
defto94 <defto94@gmail.com>
2016-03-11 00:50:17 +0100

    strzalka przewijajaca w gore, menu przepisy

--- includes/js/script.js
+++ includes/js/script.js
@@ -25,10 +25,32 @@ $(function() {
 	
 	});
 	
-	//walidacja formularza
+	//strzałka w górę
 	
+	$(document).ready(function(){
+	   $('#upArray').hide();
+	   
+	   $(window).scroll(function(){
+		  if ($(this).scrollTop() > 100) {
+			 $('#upArray').fadeIn(500);
+		  }
+		  else {
+			 $('#upArray').fadeOut(500);
+		  }
+	   });
+	   $('#upArray').click(function(){
+		  $('html, body').animate({ scrollTop: 0 }, 1000);
+		  return false;
+	   });
+	});
+	/*
+		Skrypt jQuery spełnia kilka funkcji:
 
+	ukrywa przycisk przy załadowaniu strony,
+	przy przewijaniu strony w dół wyświetla przycisk, przy powrocie do góry strony ponownie go ukrywa,
+	po kliknięciu na strzałkę przewija płynnie stronę do góry.
 
+*/
 	
 });
 	
