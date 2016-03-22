7d6e58406fb64296c6b28c5cb5a46426d403c1d0
defto94 <defto94@gmail.com>
2016-03-22 01:42:30 +0100

    Kontakty w javie - aplikacja webowa czesc pierwsza

---
+++ build/web/resources/js/script.js
@@ -0,0 +1,86 @@
+
+$(function() {
+
+	$('#sendAlert').click(function(e) {
+	
+		e.preventDefault();
+		
+		$('#successAlert').slideDown();
+	
+	});
+	
+	$("#glownaBody a:contains('Start')").parent().addClass('active');
+	$("#kontaktBody a:contains('Kontakt')").parent().addClass('active');
+	
+	//wysuwane menu jako hover
+	$('ul.nav li.dropdown').hover(function() {
+	
+		$('.dropdown-menu', this).fadeIn();
+		
+	}, function() {
+	
+		$('.dropdown-menu',this).fadeOut('fast');
+	
+	});
+	
+	//strzałka w górę
+	
+	$(document).ready(function(){
+	   $('#upArray').hide();
+	   
+	   $(window).scroll(function(){
+		  if ($(this).scrollTop() > 100) {
+			 $('#upArray').fadeIn(500);
+		  }
+		  else {
+			 $('#upArray').fadeOut(500);
+		  }
+	   });
+	   $('#upArray').click(function(){
+		  $('html, body').animate({ scrollTop: 0 }, 1000);
+		  return false;
+	   });
+	});
+	/*
+		Skrypt jQuery spełnia kilka funkcji:
+
+	ukrywa przycisk przy załadowaniu strony,
+	przy przewijaniu strony w dół wyświetla przycisk, przy powrocie do góry strony ponownie go ukrywa,
+	po kliknięciu na strzałkę przewija płynnie stronę do góry.
+
+*/
+	$(document).ready(function(){
+	
+	$(".btnInArticlesShow").click(function(){
+		$(this).hide();
+		$(".btnInArticlesHide").show();
+	});
+	
+	$(".btnInArticlesHide").click(function(){
+		$(this).hide();
+		$(".btnInArticlesShow").show();
+	});
+
+	
+	});
+	
+	//popover
+	$(function () {
+		$('[data-toggle="popover"]').popover()
+	});
+	
+	
+	$('#myModal').on('shown.bs.modal', function () {
+		$('#inputEmailLog').focus()
+	});
+	
+	$('#myModal2').on('shown.bs.modal', function () {
+		$('#inputLoginLog').focus()
+	});
+	
+	
+	
+
+});
+	
+	
