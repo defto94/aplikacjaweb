6988a5e555123a9ceca1641e60eac1e8b683c44c
Ola Śrutkowska <defto94@gmail.com>
2016-03-30 13:06:06 +0200

    Importowanie, czesc 3

--- web/resources/js/script.js
+++ web/resources/js/script.js
@@ -49,50 +49,22 @@ $(document).ready(function(){
 	po kliknięciu na strzałkę przewija płynnie stronę do góry.
 
 */
-	$(document).ready(function(){
-	
-            $(".firstShow").click(function(){
-                    $(this).hide();
-                    $("#collapseExampleOne").show(300);
-                    $(".firstHide").show();
-                    
-            });
 
-            $(".firstHide").click(function(){
-                    $(this).hide();
-                    $("#collapseExampleOne").hide(300);
-                    $(".firstShow").show();
-            });
-            
-            $(".secondShow").click(function(){
-                    $(this).hide();
-                    $("#collapseExampleSecond").show(300);
-                    $(".secondHide").show();
-            });
-
-            $(".secondHide").click(function(){
-                    $(this).hide();
-                    $("#collapseExampleSecond").hide(300);
-                    $(".secondShow").show();
-            });
-	});
 	
 	//popover
         $(function () {
-		$('[data-toggle="popover"]').popover()
-	});
-	
-	$('#myModal').on('shown.bs.modal', function () {
-		$('#inputEmailLog').focus();
-	});
-	
-	$('#myModal2').on('shown.bs.modal', function () {
-		$('#inputLoginLog').focus();
-	});
-	
-	
-	
-
+			$('[data-toggle="popover"]').popover()
+		});
+                
+                
+	$(document).ready(function(e){
+	$('#menuLogin').click(function() {
+            $.post('login.xhtml',function(xx) {
+                $('#tmpModal').html(xx)
+                $('#myModal').modal('show');
+            })
+        })
+        })
 });
 	
 	
