224dec119b5b51088461f536a85e8a445e6ee261
Ola Śrutkowska <defto94@gmail.com>
2016-05-16 00:10:36 +0200

    Konfiguracja aplikacji do wrzucenia na serwer Amazon

--- build/generated-sources/ap-source-output/entities/Recipes_.java
+++ build/generated-sources/ap-source-output/entities/Recipes_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-14T19:52:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-15T23:24:39")
 @StaticMetamodel(Recipes.class)
 public class Recipes_ { 
 
